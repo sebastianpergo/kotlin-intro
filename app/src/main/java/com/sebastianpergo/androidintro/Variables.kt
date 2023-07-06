472cd94995a6ba6ba67977e7c27c4dfd38d4eb25
sebastianpergo <sebastianperdomopg@gmail.com>
2023-07-06 17:29:12 -0500

    Variables types

--- app/src/main/java/com/sebastianpergo/androidintro/Variables.kt
+++ app/src/main/java/com/sebastianpergo/androidintro/Variables.kt
@@ -1,6 +1,30 @@
 package com.sebastianpergo.androidintro
 
 fun main(){
-    print("Hello World")
+    // Variables numericas
+
+    // Int - Does not support decimal number
+    val age:Int;
+
+    // Long
+    val example:Long = 5000000
+
+    // Float
+    val salary:Float = 40.55f
+
+    // Double
+    val saves:Double = 4565.15545
+
+    // Char - 1 character
+    var estado:Char = 'A'
+    var estadp:Char = 'I'
+
+    // String
+    var lastName:String = "Perdomo Gonzalez"
+
+    // Boolean
+    var status:Boolean = true
+
+    print("My name is " + lastName)
 }
 
